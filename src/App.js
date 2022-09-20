455fe3781bc57df441ad062be2c51218edb24c2e
slear08 <88441444+slear08@users.noreply.github.com>
2022-09-21 05:20:49 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect }from 'react';
 import Recipe from './components/Recipe';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+
+  const APPLICATION_ID = "394cb4a7";
+  const APPLICATION_KEY = "b066ba204b7377715d3db2ab3da7425f";
+
+  const [recipes, setRecipe] = useState([]);
+  const [search,setSearch] = useState("");
+  const [query,setQuery] = useState("chicken");
+
+  useEffect(() => { 
+    const getRecipe = async() => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APPLICATION_ID}&app_key=${APPLICATION_KEY}`);
+      const result = await response.json();
+      setRecipe(result.hits);
+    }
+    getRecipe();
+  },[query]);
+
+  
+  const getQuery = e =>{
+    e.preventDefault();
+    setQuery(search);
+  }
+ 
+
+  
+
   return (
     <div className="App ">
-        <div className="h-[50%]">
+        <div className="h-[50%] flex flex-row justify-center items-center text-center">
           <div className="mt-[3rem]">
-            <h1 className="text-white font-bold text-[3rem] ">WELCOME TO MY RECIPE</h1>
+            <h1 className="text-white font-bold text-[3rem] text-center">WELCOME TO MY RECIPE</h1>
             <p className="text-[1.5rem]">WE ARE HAPPY TO SERVE YOU</p>
           
-            <form className="search-form m-[3rem] ">
-              <input type="text" className="search-bar min-w-[300px] p-3 rounded-xl border-[.2rem] mr-1" placeholder="Search for your recipes for..."/>
+            <form onSubmit={getQuery} className="search-form m-[3rem] ">
+              <input type="text" className="search-bar min-w-[300px] p-3 rounded-xl border-[.2rem] mr-1" placeholder="Search for your recipes for..." value={search} onChange={e=>{setSearch(e.target.value)}}/>
               <button className="p-3 rounded-xl border-[.2rem] border-green-500 text-white mr-1 bg-green-400 hover:bg-green-600 " type="submit">SEARCH</button>
             </form>
           </div>
         </div>
-        <div className="flex flex-wrap max-w-[1500px] mx-auto">
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+        <div className="flex flex-wrap max-w-[1500px] mx-auto mb-4">
+          {recipes.map(recipe => 
+            <Recipe 
+              key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              image={recipe.recipe.image}
+              calories={recipe.recipe.calories}
+              ingredients={recipe.recipe.ingredients}
+            />
+            )}
         </div>
         <Footer />
     </div>
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export default function Recipe() {
+export default function Recipe({title, recipe,image,ingredients,calories}) {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-3">
-        <div className="md:flex">
+    <div className="w-[500px] mx-auto bg-white rounded-xl shadow-md overflow-hidden max-w-2xl m-3">
+        <div className="md:flex md:h-48 md:w-[500px] ">
             <div className="md:shrink-0">
-                <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://images.pexels.com/photos/349609/pexels-photo-349609.jpeg" alt="kithen"/>
+                <img className="h-48 w-full object-cover md:h-full md:w-48" src={image} alt="recipe"/>
             </div>
-            <div className="p-5">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">title
-            </div>
-            <p className="block mt-1 text-lg leading-tight font-medium text-black" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, atque!</p>
-            <p className="mt-2 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam libero quis nisi possimus eum fugiat cumque sit dolor quaerat repudiandae odio repellat enim, consectetur quos optio? Laudantium obcaecati alias labore?</p>
+            <div className="p-5 overflow-auto w-full">
+              <div className="uppercase tracking-wide text-xl text-indigo-500 font-semibold">{title}</div>
+              <ol className="mt-2 text-slate-500 ">
+                {ingredients.map(ingredient => (<li>{ingredient.text}</li>) )}
+              </ol>
             </div>
         </div>
     </div>
